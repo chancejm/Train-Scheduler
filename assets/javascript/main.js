3939fffa8c452d0d7248de1208315f29af653308
chancemusselman <musselman.chance@icloud.com>
2018-06-17 01:08:12 -0700

    added firebase functionality

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,9 +1,28 @@
 $("document").ready(function () {
-    console.log("***READY***");
-    console.log("MAIN.JS IS LINKED");
+    // console.log("***READY***");
+    // console.log("MAIN.JS IS LINKED");
+
+
+    // Initialize Firebase
+    config = {
+        apiKey: "AIzaSyAIVa7Dhma33UL1WeTx5nC7V2Ds08zwC8I",
+        authDomain: "homework-7-firebase.firebaseapp.com",
+        databaseURL: "https://homework-7-firebase.firebaseio.com",
+        projectId: "homework-7-firebase",
+        storageBucket: "",
+        messagingSenderId: "637674961142"
+    };
+    firebase.initializeApp(config);
+
+    database = firebase.database();
+
+    // console.log(database.ref());
+
+    
+
 
     function newTableRow(name, dest, freq, next, away) {
-        let tr = $("<tr><th scope='row'>" + name + "<td>" + dest + "</td><td>" + freq + "</td><td>" + next + "</td><td>" + away + "</td></th></tr>");
+        let tr = $("<tr><th scope='row'>" + name + "<td>" + dest + "</td><td>" + freq + "</td><td>" + next + "</td><td id='away'>" + away + "</td></th></tr>");
         $("table").append(tr);
     };
 
@@ -11,60 +30,72 @@ $("document").ready(function () {
     let dest;
     let first;
     let freq;
+    let timeConver;
+    let currentTime;
+    let timDiff;
+    let timeRemaining;
+    let timeTillTrain;
+    let nextTrain;
 
     $(".newTrainBtn").on("click", function (event) {
         event.preventDefault();
-        console.log("clicked");
+        // console.log("clicked");
         name = $("#name").val().trim();
         dest = $("#dest").val().trim();
         first = $("#first").val().trim();
         freq = $("#freq").val().trim();
-
-        var timeConvert = moment(first, "HH:mm").subtract(1, "years");
-        console.log(timeConvert);
-
-        var currentTime = moment();
-        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
-
-        var timeDiff = moment().diff(moment(timeConvert), "minutes");
-        console.log("DIFFERENCE IN TIME: " + timeDiff);
-
-        var timeRemaining = timeDiff % freq;
-        console.log(timeRemaining);
-
-        var timeTillTrain = freq - timeRemaining;
-        console.log("MINUTES TILL TRAIN: " + timeTillTrain);
-
-        var nextTrain = moment().add(timeTillTrain, "minutes");
-        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
-
-        console.log("MINUTES TILL TRAIN: " + timeTillTrain);
-        if (first.length !== 4) {
+        if (first.length !== 4 || first > 2400) {
             alert("Invalid Time");
         } else if (name && dest && first && freq) {
-
-            newTableRow(name, dest, freq, moment(nextTrain).format("hh:mma"), timeTillTrain);
             $("#name").val("");
             $("#dest").val("");
             $("#first").val("");
             $("#freq").val("");
+            database.ref().push({
+                TrainName: name,
+                Destination: dest,
+                Frequency: freq,
+                NextArrival: moment(nextTrain).format("hh:mma"),
+                FirstTrain: first
+            });
         };
+
+
     });
+
+
+database.ref().on("child_added", function(snapshot){
+
+    name = snapshot.val().TrainName;
+    dest = snapshot.val().Destination;
+    first = snapshot.val().FirstTrain;
+    freq = snapshot.val().Frequency;
+
+    timeConvert = moment(first, "HH:mm").subtract(1, "years");
+        
+    currentTime = moment();
+        
+    timeDiff = moment().diff(moment(timeConvert), "minutes");
+        
+
+        timeRemaining = timeDiff % freq;
+        
+
+        timeTillTrain = freq - timeRemaining;
+        
+
+        nextTrain = moment().add(timeTillTrain, "minutes");
+        
+
+
+    newTableRow(name, dest, freq, moment(nextTrain).format("hh:mma"), timeTillTrain);
+    
 });
 
 
-// FIREBASE INFO
-/* 
-<script src="https://www.gstatic.com/firebasejs/5.0.4/firebase.js"></script>
-<script>
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCqzmkzpBI6U0uJeotryphFcnpWzfbQqrY",
-    authDomain: "chancejm-791e1.firebaseapp.com",
-    databaseURL: "https://chancejm-791e1.firebaseio.com",
-    projectId: "chancejm-791e1",
-    storageBucket: "chancejm-791e1.appspot.com",
-    messagingSenderId: "932832111214"
-  };
-  firebase.initializeApp(config);
-</script> */
+
+
+
+});
+
+
